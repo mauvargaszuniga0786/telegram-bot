7665fb05614e8e24888fc616bfefeec6e8bce264
mauvargaszuniga0786 <mau.vargas.zuniga@gmail.com>
2026-03-03 14:43:49 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ Expiración: {expiration}
     return "ok"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
